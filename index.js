86bd79263bb986053d05a01a1bf2168f9b5e84a0
Caliza <rances.rodriguez@gmail.com>
2023-02-09 13:29:29 -0800

    Added importMarkdown, fs, and questions to index.js. Added conext/links to generateMarkdown.js.

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 // TODO: Include packages needed for this application
+const importMarkdown = require('./generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {question: 'Your project title?'},
+    {question: 'What is your description?'},
+    {question: 'What is your table of contents?'},
+    {question: 'How do you install the application?'},
+    {question: 'Usage of the application/screenshot?'},
+    {question: 'Credits/collaborators?'},
+    {question: 'Licenses?'},
+    {question: 'What is your gitHub repository name?'},
+    {question: 'What is your email?'},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, importMarkdown.generateMarkdown(data), (err) => err ? console.log(err) : console.log('Successfully created README.md'));
+};
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  ${}
+
+  ## Instulation Instructions
+
+  ${}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${data.license}
+
+  ## Contributions
+
+  ${data.contributions}
+
+  ## Test
+
+  To run tests:
+
+  \`\`\`js
+  npm run tests
+  \`\`\`
+
+  ## Questions
+  GitHub repository and email: ${data.userName} ${data.email}
 `;
 }
 
